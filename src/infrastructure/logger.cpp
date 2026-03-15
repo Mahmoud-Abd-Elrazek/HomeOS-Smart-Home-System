7897aa59b5f4782cf42cdc31595da2fdac9d8bfb
Mahmoud Abdelrazek <mahmoudzanitty@gmail.com>
2026-03-15 11:43:12 +0200

    feet(logger): Implemented operator<< for Level enum

--- src/infrastructure/logger.cpp
+++ src/infrastructure/logger.cpp
@@ -1,26 +1,17 @@
 #include "Logger.h"
-#include <iostream>
-#include <ctime>
-#include <iomanip>
 
 namespace HomeOs {
-   // Operator overloading for Level enum
-   std::ostream& operator<<(std::ostream& os, const Level& level) {
+   std::ostream& operator << (std::ostream& os, const Level& level) {
       switch (level) {
-         case Level::ERROR: os << "ERROR"; break;
+         case Level::ERROR:   os << "ERROR";   break;
          case Level::WARNING: os << "WARNING"; break;
-         case Level::INFO: os << "INFO"; break;
+         case Level::INFO:    os << "INFO";    break;
       }
       return os;
    }
 
-   void Logger::log(Level level, const std::string& sender, const std::string& message) {
-      std::time_t now = std::time(nullptr);
-      std::tm* localTime = std::localtime(&now);
-      
-      std::cout << "[" << std::put_time(localTime, "%Y-%m-%d %H:%M:%S") << "] "
-               << "[" << level << "] "
-               << "[" << sender << "]: " 
-               << message << std::endl;
+   void Logger::log(const Level& level, const std::string& sender, const std::string& message) {
+      std::cout << "[" << level << "] [" << sender << "]: " << message << "\n";
    }
+
 }
--- src/infrastructure/logger.h
+++ src/infrastructure/logger.h
@@ -1,11 +1,15 @@
 #pragma once
+#include <iostream>
 #include <string>
 
 namespace HomeOs {
-    enum class Level { INFO, WARNING, ERROR };
 
-    class Logger {
+    enum Level { INFO, WARNING, ERROR };
+
+    std::ostream& operator << (std::ostream& os, const Level& level);
+
+    class Logger { 
     public:
-        static void log(Level level, const std::string& sender, const std::string& message);
-    };
+        static void log(const Level& level, const std::string& sender, const std::string& message);
+    };      
 }
